3bae7c5c036cf42d229d8eb9bab0d171a78670f2
anassultan <anassultanali@gmail.com>
2025-03-21 06:02:34 +0300

    Route the  Create and Store Methods and the create file

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ use PHPUnit\Framework\Attributes\PostCondition;
 
  
 Route::get('/posts' , [PostController::class ,'index'])->name('posts.index');
+Route::get('posts/create',[PostController::class , 'create'])->name('posts.create');
+Route::post('/posts' ,[PostController::class , 'store' ])->name('posts.store');
 Route::get('posts/{post}', [PostController::class , 'show'])->name('posts.show');
+
+
